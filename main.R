4dfac60ba8308f1a4c449c9636a1dbb0517b4d59
Seth Caldwell <caldwellst@gmail.com>
2020-05-11 09:51:18 +0100

    Add wrapper function

--- main.R
+++ main.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(jsonlite)
 
 # Getting available indicators
+# It returns a list of length 2, with what we want as the second element
 sets <- jsonlite::fromJSON("https://ghoapi.azureedge.net/api/")
 sets <- sets[[2]]
 
@@ -15,3 +16,18 @@ df <- tibble(df[[2]]) %>%
   mutate(Value = as.numeric(Value))
 
 df
+
+# Simple function
+get_data <- function(indicator) {
+  if (!(indicator %in% sets$name)) {
+    stop(sprintf("%s is not in the indicator database.", indicator))
+  }
+  ret <- jsonlite::fromJSON(paste0("https://ghoapi.azureedge.net/api/", indicator))
+  ret[[2]]
+}
+
+# SDGTOBACCO test
+get_data("SDGTOBACCO")
+
+# FAKE TEST
+get_data("fake")
